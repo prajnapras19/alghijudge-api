69cd7ad0d225448edd4008cf7b0fdb5cfb228224
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2020-11-26 21:07:24 +0700

    added lab 3 tc

--- controllers/submitcode.js
+++ controllers/submitcode.js
@@ -3,6 +3,7 @@ const {java} = require('compile-run');
 numberOfCasesDict = {
     "TP-1 SDA 2020": 15,
     "LAB-2 SDA 2020": 16,
+    "LAB-3 SDA 2020": 10,
 }
 
 const handleSubmitCode = (fs, submitRecord) => (req, resp) => {
